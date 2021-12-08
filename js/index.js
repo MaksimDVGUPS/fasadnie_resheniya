4b35478e1867e5e8761d0f3352eebcfe5add3737
Strashko Maksim <maksim000072@gmail.com>
2021-12-08 21:48:07 +1000

    v1.0

--- js/index.js
+++ js/index.js
@@ -4,15 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Открытие модального окна с формой
     const allModals = document.querySelectorAll('.modal')
-    const leadModal = document.querySelector('#leadModal')
-    const modalButtons = document.querySelectorAll('.nav__item-button, .home__button, .specials__card, .technology__master, .technology__buy')
     const modalClose = document.querySelectorAll('.modal__close')
-
-    for (const button of modalButtons) {
-        button.addEventListener('click', () => {
-            leadModal.classList.add('modal-active')
-        })
-    }
 
     // Увеличение фото из галереи
     const galleryModal = document.querySelector('#galleryModal')
@@ -62,92 +54,87 @@ document.addEventListener('DOMContentLoaded', () => {
     const phoneInputs = document.querySelectorAll('input[type="tel"]')
     phoneMask.mask(phoneInputs) // Initial call
 
-    // Отправка формы
-    const form = document.querySelector('.modal__form')
+    // Отправка форм
+    const forms = document.querySelectorAll('.modal__form')
 
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault()
+    for (const form of forms) {
+        form.addEventListener('submit', async function (e) {
+            e.preventDefault()
 
-        // Удаление старых ошибок
-        const oldErrors = form.querySelectorAll('.modal__error')
-        if (oldErrors) {
-            for (const oldError of oldErrors) {
-                oldError.parentElement.removeChild(oldError)
+            // Удаление старых ошибок
+            const oldErrors = form.querySelectorAll('.modal__error')
+            if (oldErrors) {
+                for (const oldError of oldErrors) {
+                    oldError.parentElement.removeChild(oldError)
+                }
             }
-        }
 
-        // Валидация
-        const inputs = form.querySelectorAll('input')
-        let hasErrors = false
+            // Валидация
+            const inputs = form.querySelectorAll('input')
+            let hasErrors = false
 
-        for (const input of inputs) {
-            switch (input.name) {
-                case "name":
-                    if (input.value.length < 3) {
-                        createFormError('Слишком короткое имя', input)
-                        hasErrors = true
+            for (const input of inputs) {
+                switch (input.name) {
+                    case "name":
+                        if (input.value.length < 3) {
+                            createFormError('Слишком короткое имя', input)
+                            hasErrors = true
+                            break
+                        }
+                        if (input.value.length > 15) {
+                            createFormError('Слишком длинное имя', input)
+                            hasErrors = true
+                            break
+                        }
+                        if (!/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u.test(input.value)) {
+                            createFormError('Некорректное имя', input)
+                            hasErrors = true
+                            break
+                        }
+                        break;
+                    case "phone":
+                        if (input.value.includes('_') || !input.value) {
+                            createFormError('Введите корректный телефон', input)
+                            hasErrors = true
+                        }
                         break
-                    }
-                    if (input.value.length > 15) {
-                        createFormError('Слишком длинное имя', input)
-                        hasErrors = true
+                    case "agree":
+                        if (!input.checked) {
+                            createFormError('Нажмите галочку', input)
+                            hasErrors = true
+                        }
                         break
-                    }
-                    if (!/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u.test(input.value)) {
-                        createFormError('Некорректное имя', input)
-                        hasErrors = true
+                    default:
                         break
-                    }
-                    break;
-                case "phone":
-                    if (input.value.includes('_') || !input.value) {
-                        createFormError('Введите корректный телефон', input)
-                        hasErrors = true
-                    }
-                    break
-                case "agree":
-                    if (!input.checked) {
-                        createFormError('Нажмите галочку', input)
-                        hasErrors = true
-                    }
-                    break
-                default:
-                    break
+                }
             }
-        }
 
-        // Если прошли валидацию, добавляем дополнительные поля и отправляем форму
-        if (!hasErrors) {
-            const formData = new FormData(form)
+            // Если прошли валидацию, добавляем дополнительные поля и отправляем форму
+            if (!hasErrors) {
+                const formData = new FormData(form)
 
-            const response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData
-            })
-            if (response.ok) {
-                form.innerHTML = `
-                    <div class="modal__thx">Спасибо за то, что отправили заявку! В ближайшее время наш менеджер с Вами свяжется!</div>
-                `
-            } else {
-                alert('Что-то пошло не так... Повторите попытку позже.')
-            }
-        }
-    })
+                if (this.id === 'calculatorModalForm') {
+                    formData.append('calc_glass-type', calculatorFormData.get('calc_glass-type'))
+                    formData.append('calc_fasad', calculatorFormData.get('calc_fasad'))
+                    formData.append('calc_door-type', calculatorFormData.get('calc_door-type'))
+                    formData.append('calc_doors-count', calculatorFormData.get('calc_doors-count'))
+                    formData.append('calc_width', calculatorFormData.get('calc_width'))
+                    formData.append('calc_height', calculatorFormData.get('calc_height'))
+                    if (calculatorFormData.get('calc_additional[]')) {
+                        formData.append('calc_additional[]', calculatorFormData.getAll('calc_additional[]'))
+                    }
+                }
 
-    // Переключение вкладок с вариантами остекления
-    const tabs = document.querySelectorAll('.variants__tab')
-    const technologies = document.querySelectorAll('.technology')
-
-    for(const tabEl of tabs) {
-        tabEl.addEventListener('click', () => {
-            for(const tab of tabs) {
-                tab.classList.remove('variants__tab-active')
-            }
-            tabEl.classList.add('variants__tab-active')
-            for (const technology of technologies) {
-                technology.classList.remove('technology-active')
-                if(tabEl.id === technology.id) {
-                    technology.classList.add('technology-active')
+                const response = await fetch('sendmail.php', {
+                    method: 'POST',
+                    body: formData
+                })
+                if (response.ok) {
+                    form.innerHTML = `
+                        <div class="modal__thx">Спасибо за то, что отправили заявку! В ближайшее время наш менеджер с Вами свяжется!</div>
+                    `
+                } else {
+                    alert('Что-то пошло не так... Повторите попытку позже.')
                 }
             }
         })
@@ -168,18 +155,82 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+// Форма для расчета стоимости
+$('.header__callback, .home__btn, .technology .btn-outline').on('click', () => {
+    $('#callbackModal').addClass('modal-active')
+})
+
+// Слайдеры
 $('.photo-slider__container').slick({
     arrows: true,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
 });
 
 $('.profile__slider').slick({
     arrows: true,
     infinite: true,
     slidesToShow: 7,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 6
+            }
+        },
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 5
+            }
+        },
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        },
+    ]
 });
 
 $('.certificates__slider').slick({
@@ -188,7 +239,39 @@ $('.certificates__slider').slick({
     dots: true,
     infinite: true,
     slidesToShow: 6,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 5
+            }
+        },
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
 });
 
 $('.partners__slider').slick({
@@ -198,7 +281,22 @@ $('.partners__slider').slick({
     rows: 2,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2
+            }
+        }
+    ]
 });
 
 // Отображение ошибки в форме
@@ -208,3 +306,110 @@ function createFormError (text, where) {
     error.textContent = text
     where.insertAdjacentElement('beforebegin', error)
 }
+
+// Скрипты для калькулятора
+// Матрица путей к картинкам
+const calcSrc = [
+    ['img/calc_size-1.svg', 'img/calc_size-3.svg', 'img/calc_size-5.svg', 'img/calc_size-7.svg'],
+    ['img/calc_size-2.svg', 'img/calc_size-4.svg', 'img/calc_size-6.svg', 'img/calc_size-8.svg']
+]
+
+// Начальные индексы
+let glassType = 0
+let fasadType = 0
+
+const calcCountHTML = `
+    <div class="calculator__count">
+        <span>Кол-во:</span>
+        <input type="number" min="1" step="1" placeholder="шт." name="calc__doors-count" required>
+    </div>
+`
+
+// Клик по radio инпуту
+$('.calculator__radios input[type=radio]').on('input', function () {
+    const inputName = $(this).attr('name')
+    const inputPosition = $(this).index()
+
+    // Проверяем название инпута
+    if (inputName === 'calc_glass-type') {
+        // Проверяем его номер
+        switch (inputPosition) {
+            case 0:
+                glassType = 0
+                break
+
+            case 2:
+                glassType = 1
+                break
+
+            default:
+                break
+        }
+    } else if (inputName === 'calc_fasad') {
+        switch (inputPosition) {
+            case 0:
+                fasadType = 0
+                break
+
+            case 2:
+                fasadType = 1
+                break
+
+            case 4:
+                fasadType = 2
+                break
+
+            case 6:
+                fasadType = 3
+                break
+
+            default:
+                break
+        }
+    } else if (inputName === 'calc_door-type') {
+        $('.calculator__count').remove()
+        $(this).next().append(calcCountHTML)
+    }
+
+    // Меняем картинку
+    $('.calculator__size img').attr('src', calcSrc[glassType][fasadType])
+})
+
+// Данные из калькулятора
+let calculatorFormData = new FormData()
+
+// HTML для ошибки
+const calculatorError = `<p class="calculator__error">Выберите один из вариантов</p>`
+
+// Обработчик Submit на калькуляторе
+$('.calculator__form').on('submit', function (e) {
+    e.preventDefault()
+
+    $('.calculator__error').remove()
+
+    let errors = 0
+
+    calculatorFormData = new FormData($(this)[0])
+
+    // Валидация
+    if (!calculatorFormData.get('calc_glass-type')) {
+        $('#calc_glass-type-1').parent().parent().append(calculatorError)
+        errors++
+    }
+
+    if (!calculatorFormData.get('calc_fasad')) {
+        $('#calc_fasad-1').parent().parent().append(calculatorError)
+        errors++
+    }
+
+    if (!calculatorFormData.get('calc_door-type')) {
+        $('#calc_door-type-1').parent().parent().append(calculatorError)
+        errors++
+    }
+
+    if (errors !== 0) {
+        return false
+    }
+
+    $('#calculatorModal').addClass('modal-active')
+})
--- sendmail.php
+++ sendmail.php
@@ -13,7 +13,8 @@
     //От кого письмо
     $mail->setFrom('site@fasad.ru');
     //Кому отправить
-    $mail->addAddress('Ist.3184082@gmail.com');
+//     $mail->addAddress('Ist.3184082@gmail.com');
+    $mail->addAddress('maksim000072@gmail.com');
     //Тема письма
     $mail->Subject = 'Новая заявка с сайта фасадные-решения-мск.рф';
 
@@ -25,6 +26,17 @@
     }
     if(trim(!empty($_POST['phone']))){
         $body .= '<p><strong>Номер телефона:</strong> '.$_POST['phone'].'</p>';
+    }
+
+    if(trim(!empty($_POST['calc_glass-type']))){
+        $body .= '<h2>Данные для расчета стоимости</h2>';
+        $body .= '<p><strong>Тип остекления:</strong> '.$_POST['calc_glass-type'].'</p>';
+        $body .= '<p><strong>Вид фасада:</strong> '.$_POST['calc_fasad'].'</p>';
+        $body .= '<p><strong>Тип створок:</strong> '.$_POST['calc_door-type'].'</p>';
+        $body .= '<p><strong>Количество створок:</strong> '.$_POST['calc_doors-count'].'</p>';
+        $body .= '<p><strong>Ширина:</strong> '.$_POST['calc_width'].'</p>';
+        $body .= '<p><strong>Высота:</strong> '.$_POST['calc_height'].'</p>';
+        $body .= '<p><strong>Дополнительная комплектация:</strong> '.$_POST['calc_additional[]'].'</p>';
     }
 
     $mail->Body = $body;
